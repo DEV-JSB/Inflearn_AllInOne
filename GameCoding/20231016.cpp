2e9068c2ecb95d5d659b4ec4ae5d828694e97ca8
sujo <103971614+DEV-JSB@users.noreply.github.com>
2023-10-25 23:11:59 +0900

    20231025
    멤버변수초기화 실습

--- GameCoding/20231016.cpp
+++ GameCoding/20231016.cpp
@@ -1,7 +1,52 @@
 #include "Player2.h"
+#include <iostream>
+
+
+class Inventory
+{
+public:
+	Inventory()
+	{
+		std::cout << "Inventory()";
+
+	}
+	~Inventory()
+	{
+		std::cout << "~Inventory()";
+
+	}
+};
+class Player
+{
+public:
+	Player()
+	{
+		std::cout << "Player()";
+	}
+	virtual ~Player()
+	{
+		std::cout << "~Player()";
+	}
+};
+
+class Knight : Player
+{
+public:
+	Knight()
+	{
+		std::cout << "Knight()";
+	}
+	virtual ~Knight()
+	{
+		std::cout << "~Knight()";
+	}
+public:
+	int a = 0;
+	Inventory inven;
+};
 
 void main()
 {
-	Player2 player;
+	Knight knight;
 
 }
