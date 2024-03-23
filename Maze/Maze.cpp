a683d93a9e7a98aa600f7203e98c621468d40ac6
sujo <103971614+DEV-JSB@users.noreply.github.com>
2024-03-23 18:07:18 +0900

    Vector 학습

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -1,12 +1,19 @@
 #include "List.h"
-
+#include "Vector.h"
 void main()
 {
-	List list;
+	/*List list;
 
 	list.AddAtHead(1);
 	list.AddAtHead(2);
 	list.AddAtHead(3);
 	list.AddAtHead(4);
-	list.Print();
+	list.Print();*/
+
+
+	Vector v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+
 }
---
+++ Maze/Vector.h
@@ -0,0 +1,91 @@
+#pragma once
+#include<assert.h>
+
+class Vector
+{
+	using T = int;
+public:
+
+	explicit Vector()
+	{
+	}
+
+	~Vector()
+	{
+		if(_buffer)
+			delete[] _buffer;
+	}
+
+	void clear()
+	{
+		if (_buffer)
+		{
+			delete[] _buffer;
+			_buffer = new T[_capacity];
+		}
+		_size = 0;
+	}
+
+	void push_back(const T& data)
+	{
+		if (_size == _capacity)
+		{
+			// 증설 작업
+			int newCapacity = static_cast<int>(_capacity * 1.5f);
+			if (newCapacity == _capacity)
+				++newCapacity;
+
+			reserve(newCapacity);
+		}
+		_buffer[_size++] = data;
+		++_size;
+	}
+
+	void pop_back()
+	{
+		//TODO : 소멸
+		_size;
+	}
+
+	T& back()
+	{
+		return _buffer[_size - 1];
+	}
+
+	void reserve(int capacity)
+	{
+		if (_capacity >= capacity)
+			return;
+		_capacity = capacity;
+
+		T* newData = new T[_capacity];
+
+		for (int i = 0; i < _size; ++i)
+			newData[i] = _buffer[i];
+
+		if (_buffer)
+			delete[] _buffer;
+		_buffer = newData;
+	}
+
+	T& operator[](int index)
+	{
+		assert(index >= 0 && index < _size);
+		return _buffer[index];
+	}
+	int size()
+	{
+		return _size;
+	}
+	int capacity()
+	{
+		return _capacity;
+	}
+private:
+
+
+	T* _buffer = nullptr;
+	int _size = 0;
+	int _capacity = 0;
+};
+
